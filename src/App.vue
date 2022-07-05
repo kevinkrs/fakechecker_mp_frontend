cb94e4de6ed886841007bfecc0bc77275511ca92
kevinkrs7 <kraus.kevin@proton.me>
2022-07-05 20:04:05 +0200

    prettier fix

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,18 @@
   <v-app>
     <v-main>
       <HelloWorld />
+      <v-btn class="mx-2" fab dark small color="primary">
+        <v-icon dark> mdi-minus </v-icon>
+      </v-btn>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld";
+import HelloWorld from './components/HelloWorld';
 
 export default {
-  name: "App",
+  name: 'App',
 
   components: {
     HelloWorld,
