3a5255760247e1d92312a214c504ec1dfefa6b89
kevinkrs7 <kraus.kevin@proton.me>
2022-09-06 17:47:24 +0200

    added store

---
+++ src/store/index.js
@@ -0,0 +1,21 @@
+import { createApp } from 'vue'
+import { createStore } from 'vuex'
+
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    sentence: '',
+    url: '',
+    author: ''
+  },
+  mutations: {
+
+  },
+  actions: {
+    
+  }
+})
