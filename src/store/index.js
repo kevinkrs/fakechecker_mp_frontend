fb9546fce15d6f255b1052edcda84a9e925678f5
kevinkrs7 <kraus.kevin@proton.me>
2022-09-17 19:53:13 +0200

    Commit issues

---
+++ src/store/index.js
@@ -0,0 +1,30 @@
+/* eslint-disable */ 
+import { createApp } from 'vue'
+import { createStore } from 'vuex'
+
+import Vue from 'vue'
+import Vuex from 'vuex'
+import mutations from './mutations'
+import state from "./state"
+import actions from "./actions"
+import getters from "./getters"
+// import createPersistedState from 'vuex-persistedstate'
+// import * as Cookies from 'js-cookie'
+
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+  // plugins: [
+  //  createPersistedState({
+  //   getState: (key) => Cookies.getJSON(key),
+  //   setState: (key, state) => Cookies.set(key, state, { expires: 3, secure: true })
+  //  })
+  // ],
+  modules:{},
+  actions,
+  mutations,
+  state,
+  getters,
+})
+
+export default store;
